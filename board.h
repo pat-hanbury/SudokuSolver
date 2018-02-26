962f05772f16b784e1c2134724abf49ab48c0cbc
[pat-hanbury] <[patrickhanbury1@gmail.com]>
2018-02-26 18:36:53 -0500

    added conlfict matrixes and board.h function declarations to board.h

--- board.h
+++ board.h
@@ -7,10 +7,29 @@
 #define PROJ4SUDUKO_BOARD_H
 
 #include "location.h"
+#include <vector>
 
 class board {
     public:
-        
+            //member variables
+
+        //conflicts "improved Conflict Approach"
+        std::vector<std::vector<std::vector<bool>>> conflictsRows;
+        std::vector<std::vector<std::vector<bool>>> conflictsCols;
+        std::vector<std::vector<std::vector<bool>>> conflictsSqr;
+
+            //Methods
+        void print(); //prints boards and conflicts
+        bool isSolved; //checks if board is solved
+        void addValue(int value , location position); //adds value to a certain location. Updates conflicts
+        void clearValue(location position);
+        void checkConflict(int value ,location position); //checks if there is a confilct in a particular position
+    
+
+        //constructor. Reads board
+        void board();
+
+
 
 
 };
