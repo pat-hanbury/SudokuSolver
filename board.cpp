be7ea28e80736a2eb43212ab707a693593e52e4f
[pat-hanbury] <[patrickhanbury1@gmail.com]>
2018-02-26 19:04:10 -0500

    added getSqNum() function. Completed addValu() function

--- board.cpp
+++ board.cpp
@@ -3,3 +3,74 @@
 //
 
 #include "board.h"
+
+void board::addValue(int value, location position) {
+    //get coordinates
+    row = position.row;
+    col = position.column;
+    sqNum = getSqNum(position);
+
+    //add value to board data matrix
+    boardData[row][col] = value;
+
+    //update conflicts
+    conflictsCols[col][value-1] = TRUE;
+    conflictsRows[row][value-1] = TRUE;
+    conflictsSqr[sqNum][value-1] = TRUE;
+}
+
+int board::getSqNum(location position) {
+    row = position.row;
+    col = position.column;
+
+    if(row > 8 || col > 8){
+        throw "range error.";
+    }
+
+    //squares 0-2
+    if (row < 3){
+        if (col < 3){
+            return 0;
+        }
+
+        if (col > 2 && col < 6){
+            return 1;
+        }
+
+        if (col > 5){
+            return 2;
+        }
+    }
+
+    //squares 3-5
+    if (row > 2 && row < 6){
+        if (col < 3){
+            return 3;
+        }
+
+        if (col > 2 && col < 6){
+            return 4;
+        }
+
+        if (col > 5){
+            return 5;
+        }
+    }
+
+    if (row > 5){
+        if (col < 3){
+            return 6;
+        }
+
+        if (col > 2 && col < 6){
+            return 7;
+        }
+
+        if (col > 5){
+            return 8;
+        }
+    }
+}
+
+constructor
+resize vectors
--- board.h
+++ board.h
@@ -13,10 +13,12 @@ class board {
     public:
             //member variables
 
+        //matrix containing board
+        std::vector<std::vector<int>> boardData;
         //conflicts "improved Conflict Approach"
-        std::vector<std::vector<std::vector<bool>>> conflictsRows;
-        std::vector<std::vector<std::vector<bool>>> conflictsCols;
-        std::vector<std::vector<std::vector<bool>>> conflictsSqr;
+        std::vector<std::vector<bool>> conflictsRows;
+        std::vector<std::vector<bool>> conflictsCols;
+        std::vector<std::vector<bool>> conflictsSqr;
 
             //Methods
         void print(); //prints boards and conflicts
@@ -24,9 +26,8 @@ class board {
         void addValue(int value , location position); //adds value to a certain location. Updates conflicts
         void clearValue(location position);
         void checkConflict(int value ,location position); //checks if there is a confilct in a particular position
-    
-
         //constructor. Reads board
+        int getSqNum(location position); //outputs number 0-8 corresponding to a the square number of a position
         void board();
 
 
