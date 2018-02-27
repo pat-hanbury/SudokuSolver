83415aaebf5f2d38ddf4619d092d9fe7ba54574d
[pat-hanbury] <[patrickhanbury1@gmail.com]>
2018-02-26 19:36:38 -0500

    completed constructor. Made boardData a 2D matrix (not 3D)

--- board.cpp
+++ board.cpp
@@ -72,5 +72,51 @@ int board::getSqNum(location position) {
     }
 }
 
-constructor
-resize vectors
+void board::board() {
+    //resize conflict vectors
+    conflictsRows.resize(9,std::vector<bool>(9));
+    conflictsCols.resize(9,std::vector<bool>(9));
+    conflictsSqr.resize(9,std::vector<bool>(9));
+
+    //resize board vectors (FOO)
+    boardData.resize(9,std::vector<int>(9));
+
+    //get board input
+    std::ifstream fin;
+    //String variable to store the string from the read file
+    std::string store;
+    //Open the the text file
+    fin.open("sudoku1.txt");
+
+    //If statement that checks if the file is there or not
+    //Outputs a message if file is not there
+    if (!fin){
+        std::cout << "No file";
+        system("pause");
+        exit(0);
+    }
+
+    int i = 0;
+    int j = 0;
+
+    //"Z" dictates end of board
+    while(fin != "Z"){
+        //increment arithmetic
+        if (j == 9){
+            i++;
+            j = 0;
+        }
+
+        if (fin == "."){
+            boardData[i][j] = 0;
+        }
+
+        else{
+            boardData[i][j] = int(fin);
+        }
+
+        j++;
+        
+    }
+
+}
--- board.h
+++ board.h
@@ -8,6 +8,7 @@
 
 #include "location.h"
 #include <vector>
+#include <iostream>
 
 class board {
     public:
